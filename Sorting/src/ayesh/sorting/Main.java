d4dd6e927794db8462b99fe44bd106c7a9ec5e88
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-02-21 11:13:50 +0530

    Fix: Selection Sort.

--- Sorting/src/ayesh/sorting/Main.java
+++ Sorting/src/ayesh/sorting/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {9,14,3,2,43,11,58,22};
-        Insertion.Sort(arr);
+        Selection.Sort(arr);
         Printer.printArray(arr);
     }
 
--- Sorting/src/ayesh/sorting/Selection.java
+++ Sorting/src/ayesh/sorting/Selection.java
@@ -2,18 +2,20 @@ package ayesh.sorting;
 
 import ayesh.sorting.utils.Swapper;
 
-public class Insertion {
+public class Selection {
 
     public static void Sort(int[] array){
         for(int i = 0;i< array.length; i++){
+            int min = i;
             for(int j = i; j < array.length; j++){
-                if(array[i] > array[j]){
-                  Swapper.Swap(array, i, j);
+                if(array[min] > array[j]){
+                  min = j;
                 }
+            }
+            if(min != i){
+                Swapper.Swap(array, min, i);
             }
         }
     }
-
-
 
 }

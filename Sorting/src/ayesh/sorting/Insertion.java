0d1a81cf245b00edd0b7f4554f00c3dc3c120f5e
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-02-21 10:49:40 +0530

    Update: Insertion Sort Finished.

--- Sorting/src/ayesh/sorting/Insertion.java
+++ Sorting/src/ayesh/sorting/Insertion.java
@@ -1,10 +1,26 @@
 package ayesh.sorting;
 
+import ayesh.sorting.utils.Swapper;
+
 /**
  * Created by ayesh on 2/21/17.
  */
 public class Insertion {
-    public static void insertionSort(int[] array){
-        
+
+    public static void Sort(int[] array){
+
+        for(int i = 0;i< array.length; i++){
+
+            for(int j = i; j < array.length; j++){
+                if(array[i] > array[j]){
+                  Swapper.Swap(array, i, j);
+                }
+            }
+
+        }
+
     }
+
+
+
 }

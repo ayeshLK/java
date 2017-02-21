fb553c61d1e20a43dd237ba82f6050092f8b6b08
AyeshAlmeida <ayesh9206@gmail.com>
2017-02-21 22:40:34 +0530

    New: Bubble Sort,

--- Sorting/src/ayesh/sorting/Bubble.java
+++ Sorting/src/ayesh/sorting/Bubble.java
@@ -2,13 +2,13 @@ package ayesh.sorting;
 
 import ayesh.sorting.utils.Swapper;
 
-public class Insertion {
+public class Bubble {
 
     public static void Sort(int[] array){
         for(int i = 0; i < array.length; i++){
-            for(int j = i; j > 0; j--){
-                if(array[j] < array[j-1]){
-                    Swapper.Swap(array, j, j-1);
+            for(int j = 0; j < array.length - i - 1; j++){
+                if(array[j] > array[j+1]){
+                    Swapper.Swap(array, j ,j+1);
                 }
             }
         }
--- Sorting/src/ayesh/sorting/Main.java
+++ Sorting/src/ayesh/sorting/Main.java
@@ -16,6 +16,12 @@ public class Main {
         Insertion.Sort(arr2);
         Printer.printArray(arr2);
         System.out.println("");
+
+        System.out.println("Bubble Sort");
+        int[] arr3 = {9,14,3,2,43,11,58,22};
+        Insertion.Sort(arr3);
+        Printer.printArray(arr3);
+        System.out.println("");
     }
 
 }

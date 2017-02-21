4e81cfbf072e3fc8f7c6d55324a94f71a2a549a9
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-02-21 10:55:56 +0530

    Fix: Minor Refactoring.

--- Sorting/src/ayesh/sorting/Insertion.java
+++ Sorting/src/ayesh/sorting/Insertion.java
@@ -2,21 +2,16 @@ package ayesh.sorting;
 
 import ayesh.sorting.utils.Swapper;
 
-
 public class Insertion {
 
     public static void Sort(int[] array){
-
         for(int i = 0;i< array.length; i++){
-
             for(int j = i; j < array.length; j++){
                 if(array[i] > array[j]){
                   Swapper.Swap(array, i, j);
                 }
             }
-
         }
-
     }
 
 

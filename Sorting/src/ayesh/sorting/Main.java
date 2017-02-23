68357f812e7b33602f54ee8132af86fad4fc5b09
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-02-23 09:37:10 +0530

    Update: ArrayHandler, wip

--- Sorting/src/ayesh/sorting/Main.java
+++ Sorting/src/ayesh/sorting/Main.java
@@ -1,6 +1,6 @@
 package ayesh.sorting;
 
-import ayesh.sorting.utils.Printer;
+import ayesh.sorting.utils.ArrayHandler;
 
 public class Main {
 
@@ -8,30 +8,30 @@ public class Main {
         System.out.println("Selection Sort");
         int[] arr1 = {9,14,3,2,43,11,58,22};
         Selection.Sort(arr1);
-        Printer.printArray(arr1);
+        ArrayHandler.printArray(arr1);
         System.out.println("");
 
         System.out.println("Insertion Sort");
         int[] arr2 = {9,14,3,2,43,11,58,22};
         Insertion.Sort(arr2);
-        Printer.printArray(arr2);
+        ArrayHandler.printArray(arr2);
         System.out.println("");
 
         System.out.println("Bubble Sort");
         int[] arr3 = {9,14,3,2,43,11,58,22};
         Bubble.Sort(arr3);
-        Printer.printArray(arr3);
+        ArrayHandler.printArray(arr3);
         System.out.println("");
 
         System.out.println("Merge Sort");
         int[] arr4 = {9,14,3,2,43,11,58,22};
-        Printer.printArray(Merge.Sort(arr4));
+        ArrayHandler.printArray(Merge.Sort(arr4));
         System.out.println("");
 
         System.out.println("Quick Sort");
         int[] arr5 = {9,14,3,2,43,11,58,22};
         Quick.Sort(arr5);
-        Printer.printArray(arr5);
+        ArrayHandler.printArray(arr5);
         System.out.println("");
     }
 
--- Sorting/src/ayesh/sorting/utils/ArrayHandler.java
+++ Sorting/src/ayesh/sorting/utils/ArrayHandler.java
@@ -1,6 +1,6 @@
 package ayesh.sorting.utils;
 
-public class Printer {
+public class ArrayHandler {
 
     public static void printArray(int[] arr){
         for(int i = 0; i< arr.length; i++){

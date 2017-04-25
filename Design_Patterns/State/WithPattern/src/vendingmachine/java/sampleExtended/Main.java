0eef73a71e552e682312f51ddd6426aab8482800
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-04-25 19:41:26 +0530

    New: State Design Pattern Sample, wip

---
+++ Design_Patterns/State/WithPattern/src/vendingmachine/java/sampleExtended/Main.java
@@ -0,0 +1,10 @@
+package vendingmachine.java.sampleExtended;
+
+/**
+ * Created by ayesh on 4/25/17.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
+}

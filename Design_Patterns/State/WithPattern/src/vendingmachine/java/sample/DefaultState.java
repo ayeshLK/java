3cf4a47c511f1fbdad92ea95980d1cc20cf30002
Ayesh Almeida <ayeshs@hsenidmobile.com>
2017-04-26 12:10:50 +0530

    Update: Default State.

--- Design_Patterns/State/WithPattern/src/vendingmachine/java/sample/DefaultState.java
+++ Design_Patterns/State/WithPattern/src/vendingmachine/java/sample/DefaultState.java
@@ -3,9 +3,7 @@ package vendingmachine.java.sample;
 public class DefaultState implements State {
     @Override
     public void goNext(VendingMachineSmpl machine) {
-        if (machine.getMoneyAmount() < 5){
-            System.out.println("Still Collecting.");
-        }else if (machine.getMoneyAmount() >= 5 && machine.getMoneyAmount() < 10){
+        if (machine.getMoneyAmount() < 10){
             System.out.println("You need another Rs "+(10 - machine.getMoneyAmount())+" more.");
             machine.setCurrentState(new IntermediateState());
         }else {
--- Design_Patterns/State/WithPattern/src/vendingmachine/java/sample/VendingState.java
+++ Design_Patterns/State/WithPattern/src/vendingmachine/java/sample/VendingState.java
@@ -3,7 +3,10 @@ package vendingmachine.java.sample;
 public class VendingState implements State {
     @Override
     public void goNext(VendingMachineSmpl machine) {
-        System.out.println("Here is your Pepsi.");
+        if(machine.getMoneyAmount() == 10)
+            System.out.println("Here is your Pepsi.");
+        else if (machine.getMoneyAmount() > 10)
+            System.out.println("Here is your Pepsi. Your change is Rs "+(machine.getMoneyAmount() - 10)+".");
         machine.setMoneyAmount(0);
         machine.setCurrentState(new DefaultState());
     }

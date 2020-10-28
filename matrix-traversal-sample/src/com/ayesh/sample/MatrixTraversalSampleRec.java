1a2c7e81d9904cd127f469749041dd1649080d81
ayeshs <ayeshs@hsenidmobile.com>
2020-10-28 14:40:58 +0530

    Fix: Issue in recursion and backtracking.

--- matrix-traversal-sample/src/com/ayesh/sample/MatrixTraversalSampleRec.java
+++ matrix-traversal-sample/src/com/ayesh/sample/MatrixTraversalSampleRec.java
@@ -1,48 +1,42 @@
 package com.ayesh.sample;
 
 public class MatrixTraversalSampleRec {
-    public static boolean isPathExist(int[][] maze) {
-        if (maze.length == 1 && maze[0].length == 1) {
-            return maze[0][0] == 0;
-        } else if (maze[0][0] == 0){
-            if (maze[0].length > 1 && isPathExist(retrievePartialMatrix(maze, 0, 1, "Right Path"))) {
-                return true;
-            } else return maze[1].length > 1 && isPathExist(retrievePartialMatrix(maze, 1, 0, "Down Path"));
+    public static boolean isPathExist(int[][] maze, int startX, int startY) {
+        String message = String.format("Start X [%d] Start Y [%d]", startX, startY);
+        printMatrix(maze, startX, startY, message);
+        if (startX == maze.length - 1 && startY == maze[startX].length - 1) {
+            return true;
+        } else if (startY <= maze[0].length - 2 && maze[startX][startY + 1] == 0){
+            System.out.println("Into X Path");
+            return isPathExist(maze, startX, startY + 1);
+        } else if (startX <= maze.length - 2 && maze[startX + 1][startY] == 0) {
+            System.out.println("Into Y Path");
+            return isPathExist(maze, startX + 1, startY);
+        } else if (maze[startX - 1][startY] == 0) {
+            System.out.println("Backtracking - UP");
+            return isPathExist(maze, startX - 1, startY);
+        } else if (maze[startX][startY - 1] == 0) {
+            System.out.println("Backtracking - LEFT");
+            return isPathExist(maze, startX, startY -1);
         } else {
+            System.out.println("No Solution Found");
             return false;
         }
     }
 
-    public static int[][] retrievePartialMatrix(int[][] matrix, int startY, int startX, String  message) {
-        printMatrix(matrix, message);
-        int[][] updated = new int[matrix.length - startY][matrix[startY].length - startX];
-        int k = 0;
-        for (int i = startY; i < matrix.length; i++) {
-            int l = 0;
-            for (int j = startX; j < matrix[i].length; j++) {
-                int val = matrix[i][j];
-                if (l < matrix[i].length - startX) {
-                    updated[k][l++] = val;
-                }
+    private static void printMatrix(int[][] source, int startX, int startY, String message) {
+        System.out.println(message);
+        for (int i = startX; i < source.length; i++) {
+            for (int j = startY; j < source[i].length; j++) {
+                System.out.print(source[i][j] + " ");
             }
-            k++;
-        }
-        return updated;
-    }
-
-    public static void printMatrix(int[][] matrix, String message) {
-        System.out.println(String.format("Printing Matrix [%s]", message));
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
+            System.out.println("");
         }
     }
 
     public static void main(String... args) {
-        int matrix[][] = { { 0, 1, 1, 1, 1 }, { 0, 1, 1, 1, 1 }, { 0, 1, 0, 0, 0 }, { 0, 0, 0, 0, 1, 0 }, { 1, 1, 1, 1, 0 } };
-        boolean pathExist = isPathExist(matrix);
-        System.out.println(String.format("Path Exists Check from [{0,0}, {3,3}] [%b]", pathExist));
+        int matrix[][] = { { 0, 1, 1, 1, 1 }, { 0, 1, 1, 1, 1 }, { 0, 1, 0, 0, 0 }, { 0, 0, 0, 1, 0 }, { 1, 1, 1, 1, 0 } };
+        boolean pathExist = isPathExist(matrix, 0,0);
+        System.out.println(String.format("Path Exists Check from [{0,0}, {4,4}] [%b]", pathExist));
     }
 }

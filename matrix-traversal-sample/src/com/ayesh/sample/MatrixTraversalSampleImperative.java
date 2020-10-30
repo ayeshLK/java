51b5f1880cadf0989beaa2b31f1eb23cdfadb8f1
ayeshs <ayeshs@hsenidmobile.com>
2020-10-30 10:53:28 +0530

    Fix: Minor refactoring done for forward-move selection - check whether forward-move is a previous-move.

--- matrix-traversal-sample/src/com/ayesh/sample/MatrixTraversalSampleImperative.java
+++ matrix-traversal-sample/src/com/ayesh/sample/MatrixTraversalSampleImperative.java
@@ -16,23 +16,64 @@ public final class MatrixTraversalSampleImperative {
                 }
 
                 if (targetXValue == target.getxPoint() && targetYValue == target.getyPoint()) {
-                    path[pathIdx++] = target;
+                    path[pathIdx] = target;
                     CommonUtils.printArr("Test is Finished", path);
                     return true;
-                } else if (targetYValue <= maze[0].length - 2 && maze[targetXValue][targetYValue + 1] == 0) {
+                } else if (checkRightPath(maze, targetXValue, targetYValue, path, pathIdx)) {
                     path[pathIdx++] = new Point(targetXValue, targetYValue++);
-                } else if (targetXValue <= maze.length - 2 && maze[targetXValue + 1][targetYValue] == 0) {
+                } else if (checkDownStream(maze, targetXValue, targetYValue, path, pathIdx)) {
                     path[pathIdx++] = new Point(targetXValue++, targetYValue);
-                } else if (targetXValue != 0 && maze[targetXValue - 1][targetYValue] == 0) {
-                    path[pathIdx++] = new Point(targetXValue--, targetYValue);
-                } else if (targetYValue != 0 && maze[targetXValue][targetYValue - 1] == 0) {
+                } else if (checkLeftPath(maze, targetXValue, targetYValue)) {
                     path[pathIdx++] = new Point(targetXValue, targetYValue--);
+                } else if (checkUpStream(maze, targetXValue, targetYValue)) {
+                    path[pathIdx++] = new Point(targetXValue--, targetYValue);
                 } else {
                     return false;
                 }
             }
         }
         return false;
+    }
+
+    private static boolean checkRightPath(int[][] maze, int targetXValue, int targetYValue, Point[] path, int pathIdx) {
+        boolean isPreviousPoint = isNextPointIsPreviousPoint(path, pathIdx, targetXValue, targetYValue + 1);
+        System.out.println(String.format("Check Right Path - X [%d] Y [%d] isPrevPoint [%b]", targetXValue, targetYValue, isPreviousPoint));
+        return targetYValue <= maze[targetXValue].length - 2
+                && isNotBlocked(maze, targetXValue, targetYValue + 1)
+                && !isPreviousPoint;
+    }
+
+    private static boolean checkDownStream(int[][] maze, int targetXValue, int targetYValue, Point[] path, int pathIdx) {
+        boolean isPreviousPoint = isNextPointIsPreviousPoint(path, pathIdx, targetXValue + 1, targetYValue);
+        System.out.println(String.format("Check Down Stream - X [%d] Y [%d] isPrevPoint [%b]", targetXValue, targetYValue, isPreviousPoint));
+        return targetXValue <= maze.length - 2
+                && isNotBlocked(maze, targetXValue + 1, targetYValue)
+                && !isPreviousPoint;
+    }
+
+    private static boolean isNextPointIsPreviousPoint(Point[] path, int pathIdx, int nextPointX, int nextPointY) {
+        if (pathIdx > 0) {
+            Point previousPoint = path[pathIdx - 1];
+            System.out.println(String.format("Prev-Point Check [%s] X [%d] Y [%d]", previousPoint, nextPointX, nextPointY));
+            return previousPoint != null
+                    && previousPoint.getxPoint() == nextPointX && previousPoint.getyPoint() == nextPointY;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean checkLeftPath(int[][] maze, int targetXValue, int targetYValue) {
+        System.out.println(String.format("Check Left Path - X [%d] Y [%d]", targetXValue, targetYValue));
+        return targetYValue != 0 && isNotBlocked(maze, targetXValue, targetYValue - 1);
+    }
+
+    private static boolean checkUpStream(int[][] maze, int targetXValue, int targetYValue) {
+        System.out.println(String.format("Check Up Stream - X [%d] Y [%d]", targetXValue, targetYValue));
+        return targetXValue != 0 && isNotBlocked(maze, targetXValue - 1, targetYValue);
+    }
+
+    private static boolean isNotBlocked(int[][] maze, int xPoint, int yPoint) {
+        return maze[xPoint][yPoint] == 0;
     }
 
     public static void main(String... args) {
